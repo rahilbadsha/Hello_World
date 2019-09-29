07ad6ffd08309bc9b0c428ed1ead32765ebba1c8
rahil <rahil@DESKTOP-1GKQH1C>
2019-09-29 10:49:54 +0530

    Web Project
    Change-Id: I97da9056400ab95556f92f7d30bed5fa9326e26e

---
+++ Hello_World/src/com/javapapers/servlet/introduction/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.javapapers.servlet.introduction;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		out.println("<html><body>");
+		out.println("Hello World!");
+		out.println("This is from Servlet Class!");
+		out.println("<br>");
+		out.println("This is for testing from Batch file!");
+		out.println("</body></html>");
+		out.close();
+	}
+}
